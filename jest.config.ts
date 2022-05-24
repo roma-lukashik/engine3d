6ab0a358ea9e4bdd881fd635ac6983495e1b6743
ramanlukashyk <rlukashyk@evolution.com>
2022-05-24 17:42:10 +0300

    Use ts-node for running unit tests

---
+++ jest.config.ts
@@ -0,0 +1,25 @@
+import * as fs from "fs"
+import * as path from "path"
+import type { Config } from "@jest/types"
+import { pathsToModuleNameMapper, RawCompilerOptions } from "ts-jest"
+
+const testTsconfigFile = path.join(process.cwd(), "tsconfig.json")
+const tsConfigFile = fs.existsSync(testTsconfigFile) ? testTsconfigFile : undefined
+const tsConfig = tsConfigFile ? JSON.parse(fs.readFileSync(testTsconfigFile, { encoding: "utf-8" })) : {}
+const compilerOptions: RawCompilerOptions = tsConfig.compilerOptions ?? {}
+
+const config: Config.InitialOptions = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  testPathIgnorePatterns: ["node_modules", "dist"],
+  globals: {
+    "ts-jest": {
+      tsconfig: tsConfigFile,
+      diagnostics: false,
+    },
+  },
+  setupFilesAfterEnv: [path.resolve(__dirname, "tests/jest.setup.ts")],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths ?? {}, { prefix: "<rootDir>/" }),
+}
+
+export default config
---
+++ packages/utils/jest.config.ts
@@ -0,0 +1,8 @@
+import type { Config } from "@jest/types"
+import baseConfig from "../../jest.config"
+
+const config: Config.InitialOptions = {
+  ...baseConfig,
+}
+
+export default config
