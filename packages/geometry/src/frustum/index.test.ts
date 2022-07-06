471ecd3c98a2c16d245c01b61d443db4beb8fd39
ramanlukashyk <rlukashyk@evolution.com>
2022-07-06 23:10:58 +0300

    Add some frustum tests

---
+++ packages/geometry/src/frustum/index.test.ts
@@ -0,0 +1,89 @@
+import { Frustum } from "@geometry/frustum"
+import { Matrix4 } from "@math/matrix4"
+import { PI } from "@math/constants"
+import { Plane } from "@geometry/plane"
+import { Vector3 } from "@math/vector3"
+
+describe("Frustum", () => {
+  it("to be created", () => {
+    const left = new Plane(new Vector3(0.707, 0, -0.707), 0)
+    const right = new Plane(new Vector3(-0.707, 0, -0.707), 0)
+    const bottom = new Plane(new Vector3(0, 0.707, -0.707), 0)
+    const top = new Plane(new Vector3(0, -0.707, -0.707), 0)
+    const near = new Plane(new Vector3(0, 0, -1), -5)
+    const far = new Plane(new Vector3(0, 0, 1), 90)
+    const frustum = new Frustum(left, right, bottom, top, near, far)
+
+    expect(frustum.left.normal).toValueEqual([0.707, 0, -0.707])
+    expect(frustum.left.constant).toBeCloseTo(0)
+
+    expect(frustum.right.normal).toValueEqual([-0.707, 0, -0.707])
+    expect(frustum.right.constant).toBeCloseTo(0)
+
+    expect(frustum.bottom.normal).toValueEqual([0, 0.707, -0.707])
+    expect(frustum.bottom.constant).toBeCloseTo(0)
+
+    expect(frustum.top.normal).toValueEqual([0, -0.707, -0.707])
+    expect(frustum.top.constant).toBeCloseTo(0)
+
+    expect(frustum.near.normal).toValueEqual([0, 0, -1])
+    expect(frustum.near.constant).toBeCloseTo(-5)
+
+    expect(frustum.far.normal).toValueEqual([0, 0, 1])
+    expect(frustum.far.constant).toBeCloseTo(90)
+  })
+
+  it("fromProjectionMatrix", () => {
+    const matrix = Matrix4.perspective(PI / 4, 1, 1, 100)
+    const frustum = Frustum.fromProjectionMatrix(matrix)
+
+    expect(frustum.left.normal).toValueEqual([0.924, 0, -0.383])
+    expect(frustum.left.constant).toBeCloseTo(0)
+
+    expect(frustum.right.normal).toValueEqual([-0.924, 0, -0.383])
+    expect(frustum.right.constant).toBeCloseTo(0)
+
+    expect(frustum.bottom.normal).toValueEqual([0, 0.924, -0.383])
+    expect(frustum.bottom.constant).toBeCloseTo(0)
+
+    expect(frustum.top.normal).toValueEqual([0, -0.924, -0.383])
+    expect(frustum.top.constant).toBeCloseTo(0)
+
+    expect(frustum.near.normal).toValueEqual([0, 0, -1])
+    expect(frustum.near.constant).toBeCloseTo(-1)
+
+    expect(frustum.far.normal).toValueEqual([0, 0, 1])
+    expect(frustum.far.constant).toBeCloseTo(100)
+  })
+
+  it("setFromProjectionMatrix", () => {
+    const matrix = Matrix4.perspective(PI / 3, 1, 0.5, 50)
+    const frustum = new Frustum(
+      new Plane(Vector3.zero(), 0),
+      new Plane(Vector3.zero(), 0),
+      new Plane(Vector3.zero(), 0),
+      new Plane(Vector3.zero(), 0),
+      new Plane(Vector3.zero(), 0),
+      new Plane(Vector3.zero(), 0),
+    )
+    frustum.setFromProjectionMatrix(matrix)
+
+    expect(frustum.left.normal).toValueEqual([0.866, 0, -0.5])
+    expect(frustum.left.constant).toBeCloseTo(0)
+
+    expect(frustum.right.normal).toValueEqual([-0.866, 0, -0.5])
+    expect(frustum.right.constant).toBeCloseTo(0)
+
+    expect(frustum.bottom.normal).toValueEqual([0, 0.866, -0.5])
+    expect(frustum.bottom.constant).toBeCloseTo(0)
+
+    expect(frustum.top.normal).toValueEqual([0, -0.866, -0.5])
+    expect(frustum.top.constant).toBeCloseTo(0)
+
+    expect(frustum.near.normal).toValueEqual([0, 0, -1])
+    expect(frustum.near.constant).toBeCloseTo(-0.5)
+
+    expect(frustum.far.normal).toValueEqual([0, 0, 1])
+    expect(frustum.far.constant).toBeCloseTo(50)
+  })
+})
