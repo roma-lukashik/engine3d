d2dd642d537280f331ebbd769b8aa0c37861b8ed
rlukashyk <roman.lukashik@gmail.com>
2023-01-03 23:46:05 +0100

    Code refactoring

--- packages/physics/src/collision/index.ts
+++ packages/physics/src/collision/index.ts
@@ -20,10 +20,11 @@ export const detectContinuousCollision = (
   if (!expandBoxTowardMovementVector(movableBody.aabb, movementVector).collide(staticBody.aabb)) {
     return
   }
-  const separateAxes = mergeAxes(movableBody.obb.getBasis(), staticBody.obb.getBasis())
-  const expandedMovableBox = expandOBBTowardMovementVector(movableBody.obb, movementVector)
-  const pointsA = expandedMovableBox.getPoints()
-  const pointsB = staticBody.obb.getPoints()
+  const boxA = expandOBBTowardMovementVector(movableBody.obb, movementVector)
+  const boxB = staticBody.obb
+  const pointsA = boxA.getPoints()
+  const pointsB = boxB.getPoints()
+  const separateAxes = findSeparateAxes(boxA, boxB)
   const tests = []
   for (const axis of separateAxes) {
     const testResult = testAxis(axis, pointsA, pointsB, movementVector)
@@ -38,21 +39,15 @@ export const detectContinuousCollision = (
     return
   }
   const { contactNormal, penetration, sign } = tests.reduce((a, b) => a.penetration < b.penetration ? a : b)
-
-  const nearestPointsA = getNearestPoints(pointsA, contactNormal, sign ? Math.min : Math.max)
-  const contactPointsA = getContactPoints(nearestPointsA, staticBody.obb)
-
-  const nearestPointsB = getNearestPoints(pointsB, contactNormal, sign ? Math.max : Math.max)
-  const contactPointsB = getContactPoints(nearestPointsB, expandedMovableBox)
-
+  const contacts = findContacts(pointsA, boxA, pointsB, boxB, contactNormal, sign)
   return {
     contactNormal: contactNormal.clone().multiplyScalar(sign),
     penetration: penetration * (1 + EPS),
-    contactPoints: mergeContactPoints(contactPointsA, contactPointsB),
+    contactPoints: contacts,
   }
 }
 
-const expandBoxTowardMovementVector = (box: AABB, movementVector: Vector3): AABB => {
+function expandBoxTowardMovementVector(box: AABB, movementVector: Vector3): AABB {
   const v = box.min.clone()
   return box
     .clone()
@@ -60,7 +55,7 @@ const expandBoxTowardMovementVector = (box: AABB, movementVector: Vector3): AABB
     .expandByPoint(v.copy(box.max).add(movementVector))
 }
 
-const expandOBBTowardMovementVector = (obb: OBB, movementVector: Vector3): OBB => {
+function expandOBBTowardMovementVector(obb: OBB, movementVector: Vector3): OBB {
   const box = obb.clone()
   const halfVector = movementVector.clone().divideScalar(2)
   box.center.add(halfVector)
@@ -68,13 +63,25 @@ const expandOBBTowardMovementVector = (obb: OBB, movementVector: Vector3): OBB =
   return box
 }
 
-const mergeAxes = (axesA: Vector3[], axesB: Vector3[]): Vector3[] => {
-  return [...axesA, ...axesB].reduce<Vector3[]>((result, axis) => {
-    if (!result.some((v) => eq(v.dot(axis), 1))) {
-      result.push(axis)
+function findSeparateAxes(boxA: OBB, boxB: OBB): Vector3[] {
+  const axes: Vector3[] = []
+  collectSeparateAxes(boxA, axes)
+  collectSeparateAxes(boxB, axes)
+  return axes
+}
+
+function collectSeparateAxes(box: OBB, axes: Vector3[]): void {
+  box.getBasis().forEach((basis) => {
+    if (!axes.some((axis) => axesParallel(axis, basis))) {
+      axes.push(basis)
     }
-    return result
-  }, [])
+  })
+}
+
+function axesParallel(a: Vector3, b: Vector3): boolean {
+  // To check if vectors are parallel we need to compare dot product with product of axes magnitudes,
+  // but both of axes are unit vector, so we can compare just with 1.
+  return eq(a.dot(b), 1)
 }
 
 type AxisTest = {
@@ -83,7 +90,7 @@ type AxisTest = {
   sign: number
 }
 
-const testAxis = (axis: Vector3, pointsA: Vector3[], pointsB: Vector3[], direction: Vector3): AxisTest | undefined => {
+function testAxis(axis: Vector3, pointsA: Vector3[], pointsB: Vector3[], direction: Vector3): AxisTest | undefined {
   const projectionA = new Projection(pointsA, axis)
   const projectionB = new Projection(pointsB, axis)
   const left = projectionB.max - projectionA.min
@@ -96,26 +103,39 @@ const testAxis = (axis: Vector3, pointsA: Vector3[], pointsB: Vector3[], directi
   return { contactNormal: axis, penetration, sign }
 }
 
-const getNearestPoints = (
-  points: Vector3[],
+function findContacts(
+  pointsA: Vector3[],
+  boxA: OBB,
+  pointsB: Vector3[],
+  boxB: OBB,
   contactNormal: Vector3,
-  extremum: (...values: number[]) => number,
-): Vector3[] => {
-  // Distances to points toward contact normal.
-  const dots = points.map((p) => p.dot(contactNormal))
-  const extremumValue = extremum(...dots)
-  return points.filter((_, i) => eq(dots[i], extremumValue))
+  sign: number,
+): Vector3[] {
+  const contacts: Vector3[] = []
+  collectContacts(contacts, pointsA, boxB, contactNormal, sign ? Math.min : Math.max)
+  collectContacts(contacts, pointsB, boxA, contactNormal, sign ? Math.max : Math.max)
+  return contacts
 }
 
-const getContactPoints = (points: Vector3[], obb: OBB): Vector3[] => {
-  return points.filter((point) => obb.containsPoint(point))
-}
+function collectContacts (
+  existingContacts: Vector3[],
+  contactCandidates: Vector3[],
+  colliderOBB: OBB,
+  contactNormal: Vector3,
+  extremumFn: (...values: number[]) => number,
+): void {
+  const distances = contactCandidates.map((p) => p.dot(contactNormal))
+  const extremum = extremumFn(...distances)
 
-const mergeContactPoints = (pointsA: Vector3[], pointsB: Vector3[]): Vector3[] => {
-  return [...pointsA, ...pointsB].reduce<Vector3[]>((result, point) => {
-    if (!result.some((p) => p.equal(point))) {
-      result.push(point)
+  contactCandidates.forEach((point, i) => {
+    if (neq(distances[i], extremum)) {
+      return
     }
-    return result
-  }, [])
+    if (existingContacts.some((contact) => contact.equal(point))) {
+      return
+    }
+    if (colliderOBB.containsPoint(point)) {
+      existingContacts.push(point)
+    }
+  })
 }
