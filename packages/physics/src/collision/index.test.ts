a0c32a9215265c85d7af1ddb652633a5132daaad
rlukashyk <roman.lukashik@gmail.com>
2023-01-04 18:24:56 +0100

    Change collision box creation for tests

--- packages/physics/src/collision/index.test.ts
+++ packages/physics/src/collision/index.test.ts
@@ -7,34 +7,34 @@ import { Quaternion } from "@math/quaternion"
 describe("collision", () => {
   describe("detectContinuousCollision", () => {
     it("should not collide if AABBs are not intersected", () => {
-      const a = createCollisionBoxByAABB(new AABB(new Vector3(-1, 0, -1), new Vector3(1, 1, 1)))
-      const b = createCollisionBoxByAABB(new AABB(new Vector3(0, 0, 4), new Vector3(1, 1, 5)))
+      const a = createCollisionBox(
+        new Vector3(0, 0.5, 0),
+        new Vector3(1, 0.5, 1),
+      )
+      const b = createCollisionBox(
+        new Vector3(0, 0.5, 4),
+        new Vector3(1, 0.5, 1),
+      )
       expect(detectContinuousCollision(a, b, new Vector3(0, 0, 2))).toBeUndefined()
     })
 
     it("should not collide of there is an axis with no overlap", () => {
-      const a = createCollisionBoxByAABB(new AABB(
-        new Vector3(-1, 0, -1),
-        new Vector3(1, 1, 1),
-      ))
-      const b = createCollisionBoxByOBB(new OBB(
+      const a = createCollisionBox(
+        new Vector3(0, 0.5, 0),
+        new Vector3(1, 0.5, 1),
+      )
+      const b = createCollisionBox(
         new Vector3(2, 1, 2),
         new Vector3(1.41, 1, 0.705),
         Quaternion.fromAxisAngle(new Vector3(0, 1, 0), Math.PI / 4),
-      ))
+      )
       expect(detectContinuousCollision(a, b, new Vector3(0, 0, 0.5))).toBeUndefined()
     })
   })
 })
 
-function createCollisionBoxByAABB(aabb: AABB): CollisionBox {
-  return {
-    aabb,
-    obb: new OBB().fromAABB(aabb),
-  }
-}
-
-function createCollisionBoxByOBB(obb: OBB): CollisionBox {
+function createCollisionBox(center: Vector3, halfSize: Vector3, rotation = Quaternion.identity()): CollisionBox {
+  const obb = new OBB(center, halfSize, rotation)
   const aabb = new AABB()
   obb.getPoints().forEach((point) => aabb.expandByPoint(point))
   return { aabb, obb }
